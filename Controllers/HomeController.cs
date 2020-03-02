0acf0ec4d3dcdb0ef80c2d49495f949b9e9c40e2
Bao Cao <caoba200@uregina.ca>
2020-03-02 02:41:21 -0600

    Added files for login form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TaskHeroes.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
         {
             return View();
         }
---
+++ wwwroot/js/Login.js
@@ -0,0 +1,11 @@
+﻿<script type="text/javascript">
+    // Get the modal
+    var modal = document.getElementById('employee');
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
