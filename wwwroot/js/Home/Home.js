4fe5d9854646084d9db30bb4096cde622516e7b4
Bao Cao <caoba200@uregina.ca>
2020-03-22 14:12:39 -0600

    Fixed star rating system

--- wwwroot/js/Home/Home.js
+++ wwwroot/js/Home/Home.js
@@ -1,11 +1,18 @@
-﻿function displayStarRating(rating) {
+﻿function displayStarRating(rating, user) {
     const star_total = 5;
     const percentage = (rating / star_total) * 100;
-    document.querySelector('.box .stars-inner').style.width = percentage + "%";
+    document.querySelector("#star" + user).style.width = percentage + "%";
 }
 
 window.onload = function (event) {
-    displayStarRating(document.getElementById('user_review').value);
+    var user = 1;
+
+    while ($("#user_rating" + user).length > 0) {
+        var rating = document.getElementById("user_rating" + user).value;
+        console.log(rating);
+        displayStarRating(rating, user);
+        user++
+    }
     $('textarea').css('background-color', '#ffffff');
     $('textarea').css('border', 'none');
 };
