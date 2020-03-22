8da65f19cd555a9feb5f3e4d8470dfa6d36de195
Bao Cao <caoba200@uregina.ca>
2020-03-22 12:16:18 -0600

    User Profile Update

--- wwwroot/js/UserProfile/UserProfile.js
+++ wwwroot/js/UserProfile/UserProfile.js
@@ -8,9 +8,11 @@
 function initialForm() {
     $('textarea').css('background-color', '#ffffff');
     $('textarea').css('border', 'none');
-    $('#save_btn').css('display', 'none');
-    $('#save_edit_btn').css('display', 'none');
     $('input').attr('readonly', true);
+
+    // Re enable editable attributes for elements of the modal
+    $('.modal textarea').css('border', '1px solid');
+    $('.modal input').attr('readonly', false);
 }
 
 // Button Contact Clicked - Send email to the person.
@@ -19,14 +21,3 @@ function contact() {
     window.open('mailto:' + email);
 }
 
-// Button Edit Profile Clicked - make the fields editable and show the submit button
-function Edit_Profile() {
-    //$('textarea').attr('readonly', false);
-    //$('#edit_profile_button').css('display', 'none');
-    //$('#save_edit_btn').css('display', 'block');
-
-    console.log("click");
-
-    document.getElementById("email").readOnly = false;
-    document.getElementById("email").style.border = "1px solid";
-}
